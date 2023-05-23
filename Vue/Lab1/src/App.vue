dd53ba20c43fd6f11fe6e96014bddf6dabd8c19d
Muhammad Ashraf <muashrafalii@gmail.com>
2023-05-23 11:55:37 +0200

    Vue Lab 1

---
+++ Vue/Lab1/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+<h1>hello from APP.vue</h1>
+
+<formComponent/>
+</template>
+
+<script>
+
+import formComponent from './components/form.vue'
+export default {
+  name: 'App',
+  components: {
+    formComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
